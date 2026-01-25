680683b25a07d7059d2fd96b823bc2ec52e5a7e4
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-01-25 13:31:46 +0530

    Time: 2 ms (59.36%), Space: 43.5 MB (50.42%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.java
@@ -4,7 +4,7 @@ class Solution {
         int second=-1;
         for(int i=0;i<s.length();i++){
             char c=s.charAt(i);
-            if(c=='0'||c=='1'||c=='2'||c=='3'||c=='4'||c=='5'||c=='6'||c=='7'||c=='8'||c=='9'){
+            if(Character.isDigit(c)){
                 int a=c-'0';
                 if(a>first){
                     second=first;
