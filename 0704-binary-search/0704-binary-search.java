05cc0e4cc523e7a75a05655cf07677dd1c8f7b9e
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-01-29 10:09:28 +0530

    Time: 0 ms (100%), Space: 48.4 MB (42.68%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,20 +1,20 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return helper(nums,0,nums.length,target);
+        return helper(nums,0,nums.length-1,target);
     }
     public int helper(int[] arr,int left,int right,int target){
         
-        while(left<right){
+        while(left<=right){
             int mid=(left+right)/2;
             if(arr[mid]==target)return mid;
             else if(arr[mid]<target){
                 left=mid+1;
             }
             else{
-                right=mid;
+                right=mid-1;
             }
 
         }
-        return -1;
+        return 1-2;
     }
 }
