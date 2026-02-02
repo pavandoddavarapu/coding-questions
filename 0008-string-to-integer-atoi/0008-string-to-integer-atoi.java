76106415d2b3cdf3085435d624e5918120ff2d83
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-02-02 18:48:05 +0530

    Time: 4 ms (6.23%), Space: 45.3 MB (5.31%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,27 +1,25 @@
 class Solution {
     public int myAtoi(String s) {
         int i=0;
-        boolean flag=false;
-        // if(s.length()>0 && s.charAt(i)=='-'){i++;flag=true;}
-        // else if(s.length()>0 && s.charAt(i)=='+'){i++;}
-        while(i<s.length() && (s.charAt(i)==' ' || s.charAt(i)=='0')){
+        while(i<s.length() && (s.charAt(i)==' ' )){
             i++;
         }
+        boolean flag=false;
+      
         if(i!=s.length() && s.charAt(i)=='+'){i++;}
         else if(i!=s.length() && s.charAt(i)=='-'){
-            if(i-1>=0){
-                 if(s.charAt(i-1)=='0'){i--;}
-            }
             flag=true;i++;
 
         }
         int j=i;
-        int count=0;
+        long ans=0;
         while(j<s.length() && (s.charAt(j)>='0' && s.charAt(j)<='9')){
-            j++;count++;
+            j++;
+            ans=Long.parseLong('0'+s.substring(i,j));
+            if(ans>Integer.MAX_VALUE) return flag ? Integer.MIN_VALUE : Integer.MAX_VALUE;
         }
-        System.out.println(count);
-           if(count>10) return flag ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        
+         
         return flag ? -Integer.parseInt('0'+s.substring(i,j)) : Integer.parseInt('0'+s.substring(i,j));
 
     }
