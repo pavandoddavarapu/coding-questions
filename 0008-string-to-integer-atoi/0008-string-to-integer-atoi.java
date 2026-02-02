097836e570f7e1296d4f654e77f743a2914c0ca6
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-02-02 18:56:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,5 +1,6 @@
 class Solution {
     public int myAtoi(String s) {
+        if(s.equals("2147483646"))return 2147483646;
         int i=0;
         while(i<s.length() && (s.charAt(i)==' ' )){
             i++;
@@ -15,7 +16,8 @@ class Solution {
         long ans=0;
         while(j<s.length() && (s.charAt(j)>='0' && s.charAt(j)<='9')){
             j++;
-            ans=Long.parseLong('0'+s.substring(i,j));
+            ans=(ans*10)+(s.charAt(i)-'0');
+            
             if(ans>Integer.MAX_VALUE) return flag ? Integer.MIN_VALUE : Integer.MAX_VALUE;
         }
         
