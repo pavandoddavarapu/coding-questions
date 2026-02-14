89481cea24a3e4dbb2698c671c9ecdf3f96cc24a
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-02-14 22:45:08 +0530

    Time: 579 ms (6.01%), Space: 280.3 MB (9.54%) - LeetHub

---
+++ 0799-champagne-tower/0799-champagne-tower.java
@@ -0,0 +1,23 @@
+class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double ans[][]=new double[2000][2000];
+        ans[0][0]=poured;
+        int level=0;
+        while(level<query_row+1){
+           
+            for(int i=0;i<level+1;i++){
+                if(ans[level][i]>1){
+                    double a=ans[level][i]-1;
+                    poured=poured-1;
+                    ans[level][i]=1;
+                    ans[level+1][i]=ans[level+1][i]+a/2.0;
+                    ans[level+1][i+1]=ans[level+1][i+1]+a/2.0;
+                   
+                }
+            }
+            level=level+1;
+            
+        }
+        return ans[query_row][query_glass];
+    }
+}
