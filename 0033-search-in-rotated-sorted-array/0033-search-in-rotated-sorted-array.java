9ffac945d98d3bc9a43220521194dfb4dbd82506
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-02-24 22:30:43 +0530

    Time: 0 ms (100%), Space: 44.1 MB (9.8%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -6,6 +6,7 @@ class Solution {
             int mid=(start+end)/2;
             if(target==nums[mid])return mid;
             if(nums[start]<=nums[mid]){
+                
                 if(nums[start]<=target && nums[mid]>=target){
                     end=mid-1;
                 }
