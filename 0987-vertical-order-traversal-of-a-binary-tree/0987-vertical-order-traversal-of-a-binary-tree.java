372a47b461dcb665940a23be26e9a4adb2e5339b
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-02-25 22:24:39 +0530

    Time: 3 ms (94.76%), Space: 44.3 MB (37.64%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -15,43 +15,49 @@
  */
 class Solution {
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        List<List<Integer>> result= new ArrayList<>();
+        List<List<Integer>> result=new ArrayList<>();
+        if(root==null)return result;
         TreeMap<Integer,TreeMap<Integer,ArrayList<Integer>>> tmap=new TreeMap<>();
+        //column
+        //row
+        //ArrayList
         dfs(root,tmap,0,0);
-        //column wise sorting
-        for(Map.Entry<Integer,TreeMap<Integer,ArrayList<Integer>>> col : tmap.entrySet()){
-            TreeMap<Integer,ArrayList<Integer>> tm= col.getValue();
+        for(Map.Entry<Integer,TreeMap<Integer,ArrayList<Integer>>> col: tmap.entrySet()){
+            TreeMap<Integer,ArrayList<Integer>> tm=col.getValue();
             ArrayList<Integer> list=new ArrayList<>();
-            for(Map.Entry<Integer,ArrayList<Integer>> row : tm.entrySet()){
+            for(Map.Entry<Integer,ArrayList<Integer>> row: tm.entrySet()){
+
                 ArrayList<Integer> sublist=row.getValue();
                 Collections.sort(sublist);
                 list.addAll(sublist);
+
             }
             result.add(list);
+
         }
         return result;
     }
     public void dfs(TreeNode root,TreeMap<Integer,TreeMap<Integer,ArrayList<Integer>>> tmap,int i,int j){
         if(root==null)return ;
-        
         if(tmap.containsKey(i)){
-            TreeMap<Integer,ArrayList<Integer>> row=tmap.get(i);
-            if(row.containsKey(j)){
+            if(tmap.get(i).containsKey(j)){
                 tmap.get(i).get(j).add(root.val);
             }
-            else{ArrayList<Integer> ls=new ArrayList<>();ls.add(root.val);tmap.get(i).put(j,ls);}
-
+            else{
+                ArrayList<Integer> arr=new ArrayList<>();
+                arr.add(root.val);
+                tmap.get(i).put(j,arr);
+            }
         }
         else{
-            TreeMap<Integer,ArrayList<Integer>> row=new TreeMap<>();
-            ArrayList<Integer> ls=new ArrayList<>();
-            ls.add(root.val);
-            row.put(j,ls);
-            
-            tmap.put(i,row);
+            ArrayList<Integer> arr=new ArrayList<>();
+            arr.add(root.val);
+            TreeMap<Integer,ArrayList<Integer>>tp=new TreeMap<>();
+            tp.put(j,arr);
+            tmap.put(i,tp);
         }
         dfs(root.left,tmap,i-1,j+1);
         dfs(root.right,tmap,i+1,j+1);
+        return;
     }
-
 }
