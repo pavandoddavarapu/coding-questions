abc54396137eb6e4e4100a9adc3f699e1361bf36
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-03-03 08:40:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,19 +1,27 @@
 class Solution {
+    public class pair{
+        int a;
+        int b;
+        pair(int a,int b){
+            this.a=a;
+            this.b=b;
+        }
+    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int ac=image[sr][sc];
         boolean visited[][] =new boolean[image.length][image[0].length];
-        Queue<Integer> c=new LinkedList<>();
-        Queue<Integer> r=new LinkedList<>();
-        c.add(sr);
-        r.add(sc);
+        Queue<pair> c=new LinkedList<>();
+        c.add(new pair(sr,sc));
+       
         while(!c.isEmpty()){
-            int i=c.poll();
-            int j=r.poll();
+            pair p=c.poll();
+            int i=p.a;
+            int j=p.b;
             image[i][j]=color;
-            if(i-1>=0 && !visited[i-1][j] && image[i-1][j]==ac){c.add(i-1);r.add(j);visited[i-1][j]=true;}
-            if(j+1<=image[0].length-1 && !visited[i][j+1] && image[i][j+1]==ac){c.add(i);r.add(j+1);visited[i][j+1]=true;}
-            if(i+1<=image.length-1 && !visited[i+1][j] && image[i+1][j]==ac){c.add(i+1);r.add(j);visited[i+1][j]=true;}
-            if(j-1>=0 && !visited[i][j-1] && image[i][j-1]==ac){c.add(i);r.add(j-1);visited[i][j-1]=true;}
+            if(i-1>=0 && !visited[i-1][j] && image[i-1][j]==ac){c.add(new pair(i-1,j));visited[i-1][j]=true;}
+            if(j+1<=image[0].length-1 && !visited[i][j+1] && image[i][j+1]==ac){c.add(new pair(i,j+1));visited[i][j+1]=true;}
+            if(i+1<=image.length-1 && !visited[i+1][j] && image[i+1][j]==ac){c.add(new pair(i+1,j));visited[i+1][j]=true;}
+            if(j-1>=0 && !visited[i][j-1] && image[i][j-1]==ac){c.add(new pair(i,j-1));visited[i][j-1]=true;}
         }
         return image;
     }
