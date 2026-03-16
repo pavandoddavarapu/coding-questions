0fe6b2f295e47d6c18de7153808367128b0975fc
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-03-16 12:27:56 +0530

    Time: 3 ms (30.33%), Space: 47.5 MB (91.04%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,58 @@
+class Solution {
+    public class pair{
+        int a;
+        int b;
+        pair(int a,int b){
+            this.a=a;
+            this.b=b;
+        }
+    }
+
+    public void solve(char[][] board) {
+        boolean visited[][]=new boolean[board.length][board[0].length];
+        for(int i=0;i<board[0].length;i++){
+            if(!visited[0][i] && board[0][i]=='O'){bfs(0,i,visited,board);}
+            int n=board.length-1;
+            if(!visited[n][i] && board[n][i]=='O'){bfs(n,i,visited,board);}
+        }
+        for(int i=0;i<board.length;i++){
+            
+            if(!visited[i][0] && board[i][0]=='O'){bfs(i,0,visited,board);}
+            int n=board[0].length-1;
+            if(!visited[i][n] && board[i][n]=='O'){bfs(i,n,visited,board);}
+        }
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(visited[i][j]==false && board[i][j]=='O'){
+                    board[i][j]='X';
+                }
+            }
+        }
+        return ;
+    }
+
+
+    public void bfs(int i1,int j1,boolean[][] visited,char board[][]){
+        Queue<pair> q=new LinkedList<>();
+        visited[i1][j1]=true;
+        q.add(new pair(i1,j1));
+        while(!q.isEmpty()){
+            pair p=q.poll();
+            int i=p.a;
+            int j=p.b;
+            if(i-1>=0 && board[i-1][j]=='O' && !visited[i-1][j]){
+                q.add(new pair(i-1,j));visited[i-1][j]=true;
+            }
+            if(j-1>=0 && board[i][j-1]=='O' && !visited[i][j-1]){
+                q.add(new pair(i,j-1));visited[i][j-1]=true;
+            }
+            if(i+1<board.length  && board[i+1][j]=='O' && !visited[i+1][j]){
+                q.add(new pair(i+1,j));visited[i+1][j]=true;
+            }
+            if(j+1<board[0].length && board[i][j+1]=='O' && !visited[i][j+1]){
+                q.add(new pair(i,j+1));visited[i][j+1]=true;
+            }
+
+        }
+    }
+}
