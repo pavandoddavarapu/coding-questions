78bd671c13293f988cd6ab91ee76dfa408330586
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-03-31 15:38:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -14,16 +14,34 @@
  * }
  */
 class Solution {
-    int sum=0;
-    public int sumNumbers(TreeNode root) {
-        helper(root,0);
-        return sum;
+
+    public class pair{TreeNode node;int val;
+    pair(TreeNode node,int val){
+        this.node=node;
+        this.val=val;
     }
-    public void helper(TreeNode root,int value){
-        if(root==null)return;
-        if(root.left==null && root.right==null){value=value*10+root.val;sum=sum+value;}
-        int local=value*10+root.val;
-        helper(root.left,local);
-        helper(root.right,local);
+    }
+    
+    public int sumNumbers(TreeNode root) {
+        if(root==null)return 0;
+        Queue<pair> q=new LinkedList<>();
+        int sum=0;
+        q.add(new pair(root,0));
+        while(!q.isEmpty()){
+            pair p1=q.poll();
+            TreeNode p=p1.node;
+            
+            if(p.left==null && p.right==null){p1.val=p1.val*10+p.val;sum=sum+p1.val;System.out.print(p1.val+" "+sum+",");}
+            if(p.left!=null){
+                q.add(new pair(p.left,p1.val*10+p.val));
+            }
+            if(p.right!=null){
+                q.add(new pair(p.right,p1.val*10+p.val));
+            }
+
+        }
+
+        
+        return sum;
     }
 }
