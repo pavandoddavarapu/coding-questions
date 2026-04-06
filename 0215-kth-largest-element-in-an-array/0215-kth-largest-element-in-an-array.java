2cf0c0963b10430a35ee7615cd9b7cb50de6d466
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-04-06 22:35:16 +0530

    Time: 80 ms (14.26%), Space: 67.9 MB (81.12%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,19 +1,13 @@
-import java.util.PriorityQueue;
-
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        // A min-heap (default for PriorityQueue in Java)
-        PriorityQueue<Integer> pq = new PriorityQueue<>(); 
-        
-        for (int a : nums) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int a: nums){
             pq.add(a);
-            // If the heap size exceeds k, remove the smallest element (the root)
-            if (pq.size() > k) {
-                pq.poll();
-            }
         }
-        
-        // The root of the min-heap will be the k-th largest element
+        int i=0;
+        while(i<nums.length-k){
+            pq.poll();i++;
+        }
         return pq.poll();
     }
 }
