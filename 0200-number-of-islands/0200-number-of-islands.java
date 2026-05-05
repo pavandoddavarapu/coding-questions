d4355ce413adcee9b7c102d8f4c9c86d820f8470
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-05-05 11:34:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -13,7 +13,7 @@ class Solution {
             for(int j=0;j<grid[0].length;j++){
                 if(grid[i][j]=='1'){
                     count++;
-                    Queue<pair> q=new ArrayDeque<>();
+                    Queue<pair> q=new ConcurrentLinkedQueue<>();
                     pair p=new pair(i,j);
         q.add(p);
         while(!q.isEmpty()){
