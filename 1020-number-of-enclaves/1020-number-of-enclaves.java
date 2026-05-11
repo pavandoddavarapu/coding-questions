f6b2de648bd238b972ff720d6baaea2b5a6ffc7d
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-05-12 02:43:49 +0530

    Time: 8 ms (69.91%), Space: 57.3 MB (67.23%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,60 +1,43 @@
-
 class Solution {
-    public class pair{
-        int a;
-        int b;
-        pair(int a,int b){
-            this.a=a;
-            this.b=b;
+    public int numEnclaves(int[][] grid) {
+        for(int i=0;i<grid[0].length;i++){
+            if(grid[0][i]==1){
+                dfs(grid,0,i);
+            }
         }
-    }
-
-    public int numEnclaves(int[][] board) {
-        boolean visited[][]=new boolean[board.length][board[0].length];
-        for(int i=0;i<board[0].length;i++){
-            if(!visited[0][i] && board[0][i]==1){bfs(0,i,visited,board);}
-            int n=board.length-1;
-            if(!visited[n][i] && board[n][i]==1){bfs(n,i,visited,board);}
+        for(int i=0;i<grid.length;i++){
+            if(grid[i][grid[0].length-1]==1){
+                dfs(grid,i,grid[0].length-1);
+            }
         }
-        for(int i=0;i<board.length;i++){
-            
-            if(!visited[i][0] && board[i][0]==1){bfs(i,0,visited,board);}
-            int n=board[0].length-1;
-            if(!visited[i][n] && board[i][n]==1){bfs(i,n,visited,board);}
+        for(int i=0;i<grid[0].length;i++){
+            if(grid[grid.length-1][i]==1){
+                dfs(grid,grid.length-1,i);
+            }
+        }
+        for(int i=0;i<grid.length;i++){
+            if(grid[i][0]==1){
+                dfs(grid,i,0);
+            }
         }
         int count=0;
-        for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++){
-                if(visited[i][j]==false && board[i][j]==1){
-                    count++;
-                }
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1)count++;
             }
         }
         return count;
     }
-
-
-    public void bfs(int i1,int j1,boolean[][] visited,int board[][]){
-        Queue<pair> q=new LinkedList<>();
-        visited[i1][j1]=true;
-        q.add(new pair(i1,j1));
-        while(!q.isEmpty()){
-            pair p=q.poll();
-            int i=p.a;
-            int j=p.b;
-            if(i-1>=0 && board[i-1][j]==1 && !visited[i-1][j]){
-                q.add(new pair(i-1,j));visited[i-1][j]=true;
+    public void dfs(int grid[][],int i,int j){
+        grid[i][j]=0;
+        int iers[]={-1,+1,0,0};
+        int jers[]={0,0,-1,+1};
+        for(int k=0;k<4;k++){
+            int m=i+iers[k];
+            int n=j+jers[k];
+            if(m>=0 && n>=0 && m<grid.length && n<grid[0].length && grid[m][n]==1 ){
+                dfs(grid,m,n);
             }
-            if(j-1>=0 && board[i][j-1]==1 && !visited[i][j-1]){
-                q.add(new pair(i,j-1));visited[i][j-1]=true;
-            }
-            if(i+1<board.length  && board[i+1][j]==1 && !visited[i+1][j]){
-                q.add(new pair(i+1,j));visited[i+1][j]=true;
-            }
-            if(j+1<board[0].length && board[i][j+1]==1 && !visited[i][j+1]){
-                q.add(new pair(i,j+1));visited[i][j+1]=true;
-            }
-
         }
     }
 }
