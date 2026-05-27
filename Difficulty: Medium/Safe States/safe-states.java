1868799c0d8dcf8f1b61fe7fa53b8fbcc94e1fd2
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-05-27 16:59:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Safe States/safe-states.java
@@ -0,0 +1,35 @@
+class Solution {
+    public ArrayList<Integer> safeNodes(int V, int[][] edges) {
+        // Code here
+        ArrayList<ArrayList<Integer>> arr=new ArrayList<>();
+        for(int i=0;i<V;i++){
+            arr.add(new ArrayList<Integer>());
+        }
+        for(int i=0;i<edges.length;i++){
+            arr.get(edges[i][0]).add(edges[i][1]);
+        }
+        boolean vis[]=new boolean[V];
+        boolean path_vis[]=new boolean[V];
+        ArrayList<Integer> ans=new ArrayList<Integer>();
+        for(int i=0;i<V;i++){
+            if(vis[i]==false){dfs(i,vis,path_vis,arr);}
+            if(path_vis[i]==false)ans.add(i);
+            
+        }
+        return ans;
+    }
+    public boolean dfs(int v,boolean vis[],boolean path_vis[],ArrayList<ArrayList<Integer>> arr){
+        vis[v]=true;
+        path_vis[v]=true;
+        int n=arr.get(v).size();
+        for(int i=0;i<n;i++){
+            int a=arr.get(v).get(i);
+            if(vis[a]==false){
+                if(dfs(a,vis,path_vis,arr)==true)return true;
+            }
+            else if(path_vis[a]==true)return true;
+        }
+        path_vis[v]=false;
+        return false;
+    }
+}
