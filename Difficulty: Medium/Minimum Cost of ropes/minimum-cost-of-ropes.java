2cef6853ceee3b3038945326f00d559f2b2faff9
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-05-30 10:46:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Cost of ropes/minimum-cost-of-ropes.java
+++ Difficulty: Medium/Minimum Cost of ropes/minimum-cost-of-ropes.java
@@ -2,16 +2,17 @@ class Solution {
     public static int minCost(int[] arr) {
         // code here
         PriorityQueue<Integer> pq=new PriorityQueue<>();
-        for(int a: arr){
-            pq.add(a);
+        for(int i=0;i<arr.length;i++){
+            pq.add(arr[i]);
         }
-        int ans=0;
+        int cost=0;
+        
         while(pq.size()>1){
             int a=pq.poll();
             int b=pq.poll();
-            ans=ans+a+b;
             pq.add(a+b);
+            cost=cost+a+b;
         }
-        return ans;
+        return cost;
     }
 }
