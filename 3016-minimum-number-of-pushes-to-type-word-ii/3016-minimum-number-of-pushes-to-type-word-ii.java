6f3fee2eea7423b3a759923148a34d324c537876
pavan doddavarapu <145573517+pavandoddavarapu@users.noreply.github.com>
2026-09-16 09:18:34 +0530

    Time: 93 ms (5.59%), Space: 48.1 MB (35.98%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -1,20 +1,27 @@
 class Solution {
     public class pair{
-        
+        int frq;
+        Character c;
+        pair(int f,char ch){
+            this.frq=f;
+            this.c=ch;
+        }
     }
     public int minimumPushes(String word) {
         char[] ch=word.toCharArray();
-        int ans[]=new int[26];
+        HashMap<Character,Integer> hm=new HashMap<>();
         for(char c:ch){
-            ans[c-'a']++;
+            hm.put(c,hm.getOrDefault(c,0)+1);
         }
-        
-        Arrays.sort(ans);
+        ArrayList<pair> arr=new ArrayList<>();
+        for(Map.Entry<Character,Integer> entry:hm.entrySet()){
+            arr.add(new pair(entry.getValue(),entry.getKey()));
+        }
+        Collections.sort(arr,(a,b)->b.frq-a.frq);
         int k=0;
         int count=0;
-        for(int i=25;i>=0;i--){
-            int m=ans[i];
-            if(ans[i]==0)continue;
+        for(int i=0;i<arr.size();i++){
+            int m=arr.get(i).frq;
             if(k<8){
                 count=count+m;
                 k++;
@@ -31,7 +38,7 @@ class Solution {
                 count=count+(m*4);
                 k++;
             }
-
+            System.out.print(count+" ");
         }
         return count;
     }
